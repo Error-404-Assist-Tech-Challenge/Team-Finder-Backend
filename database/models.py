c98274524a4809bff5b90cd18545d23b5e6ecc47
vladono3 <50675017+vladono3@users.noreply.github.com>
2024-02-24 18:09:45 +0200

    Added Organization Administator Role

--- database/models.py
+++ database/models.py
@@ -25,3 +25,26 @@ class Employee(Base):
                 "password": str(employee.password)
             }
         return serialized_employees
+
+class Admin(Base):
+    __tablename__ = "admin"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    oname = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    @staticmethod
+    def serialize_admin(admins):
+        serialize_admin = {}
+        for admin in admins:
+            serialize_admin[str(admin.id)] = {
+                "id": str(admin.id),
+                "name": str(admin.name),
+                "email": str(admin.email),
+                "password": str(admin.password),
+                "oname": str(admin.oname),
+                "address": str(admin.address)
+            }
+        return serialize_admin
