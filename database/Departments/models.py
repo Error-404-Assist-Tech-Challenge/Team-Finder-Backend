78f52e8f4b61c52e2b55c513284264ae1bd51c34
vladutvoicu <vladut.d.voicu@gmail.com>
2024-02-28 18:49:28 +0200

    refactor users folder name

--- database/Departments/models.py
+++ database/Departments/models.py
@@ -8,8 +8,8 @@ class Department_members(Base):
     __tablename__ = "department_members"
 
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
-    dept_id = Column(UUID(as_uuid=True), ForeignKey("departments.id"),nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"),nullable=False)
+    dept_id = Column(UUID(as_uuid=True), ForeignKey("departments.id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     @staticmethod
     def serialize_department_members(members):
         serialize_department_members = {}
@@ -26,9 +26,9 @@ class Department(Base):
     __tablename__ = "departments"
 
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
-    org_id = Column(UUID(as_uuid=True), ForeignKey("organizations.id"),nullable=False)
+    org_id = Column(UUID(as_uuid=True), ForeignKey("organizations.id"), nullable=False)
     name = Column(String, nullable=False)
-    manager_id = Column(UUID(as_uuid=True), ForeignKey("users.id"),nullable=False)
+    manager_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     created_at = Column(TIMESTAMP, nullable=False)
 
     @staticmethod
