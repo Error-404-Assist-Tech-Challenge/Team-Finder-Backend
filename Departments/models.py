e97b8ce0338fcb287482b09f77b97b543f2e27d5
vladutvoicu <vladut.d.voicu@gmail.com>
2024-03-06 14:47:04 +0200

    changed department response

--- Departments/models.py
+++ Departments/models.py
@@ -1,11 +1,41 @@
-from typing import Optional
+from typing import Optional, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel,  validator
 from uuid import UUID
 from datetime import datetime
 
 
+# DEPARTMENT_MEMBERS
+class DepartmentMember(BaseModel):
+    user_id: UUID
+
+
+class Delete_member(BaseModel):
+    user_id: UUID
+
+
+class DepartmentMemberResponse(BaseModel):
+    user_id: UUID
+    dept_id: UUID
+    user_name: str
+
+
 # DEPARTMENTS
+class DepartmentResponse(BaseModel):
+    id: UUID
+    org_id: UUID
+    name: str
+    manager_id: Optional[UUID]
+    created_at: str
+    department_members: List[DepartmentMemberResponse]
+    manager_name: str
+
+    @validator('manager_id', pre=True)
+    def validate_manager_id(cls, value):
+        if isinstance(value, str) and value.lower() == 'none':
+            return None
+        return value
+
 class Department(BaseModel):
     name: str
     created_at: datetime = datetime.now().isoformat()
@@ -19,12 +49,3 @@ class DepartmentUpdate(BaseModel):
 
 class Remove_department(BaseModel):
     dept_id: UUID
-
-
-class Delete_member(BaseModel):
-    user_id: UUID
-
-
-# DEPARTMENT_MEMBERS
-class DepartmentMember(BaseModel):
-    user_id: UUID
