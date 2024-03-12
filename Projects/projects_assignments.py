d08af03f2807576ae1c5f6e8c74f1dba4d6647d8
vladutvoicu <vladut.d.voicu@gmail.com>
2024-03-12 17:36:17 +0200

    added accept/reject project proposal endpoints

--- Projects/projects_assignments.py
+++ Projects/projects_assignments.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from Projects.models import AssignmentProposal, UpdateAssignmentProposal, SearchResponse, DeleteAssignmentProposal
-from Projects.utils import create_project_assignment, get_project_assignments, update_project_assignment, delete_project_assignment
+from Projects.models import AssignmentProposal, UpdateAssignmentProposal, SearchResponse, DeleteAssignmentProposal, ManageProposal
+from Projects.utils import create_project_assignment, get_project_assignments, update_project_assignment, delete_project_assignment, manage_proposal
 from auth import AuthHandler
 
 project_assignments_router = APIRouter()
@@ -20,6 +20,11 @@ def create_project_assignments_route(project_assignments_data: UpdateAssignmentP
 @project_assignments_router.delete("/api/projects/assignment_proposal", response_model=SearchResponse)
 def create_project_assignments_route(project_assignments_data: DeleteAssignmentProposal, user_id: str = Depends(auth_handler.auth_wrapper)):
     return delete_project_assignment(project_assignments_data, user_id)
+
+
+@project_assignments_router.post("/api/projects/manage_proposal")
+def manage_proposal_route(manage_proposal_data: ManageProposal, user_id: str = Depends(auth_handler.auth_wrapper)):
+    return manage_proposal(manage_proposal_data, user_id)
 
 
 @project_assignments_router.get("/api/projects/project_assignments")
