c3edaa4dd51f8a61b6cefd61814b6c3b02f56035
vladutvoicu <vladut.d.voicu@gmail.com>
2024-05-08 16:06:26 +0300

    updated messages utils and endpoints

--- Messages/messages.py
+++ Messages/messages.py
@@ -8,34 +8,34 @@ auth_handler = AuthHandler()
 message_router = APIRouter()
 
 
-@message_router.post("/api/messages", response_model=ResponseMessages)
-async def create_message(message_data: Messages, user_id: str = Depends(auth_handler.auth_wrapper)):
+@message_router.post("/api/messages", response_model=List[MessageResponse])
+async def create_message(message_data: Message, user_id: str = Depends(auth_handler.auth_wrapper)):
     discussion_id = message_data.discussion_id
 
-    discussion, error = db.get_discussions()
+    discussion, error = db.get_discussion(discussion_id)
     if not discussion:
-        raise HTTPException(status_code=404, detail="Discussion not found.")
+        raise HTTPException(status_code=404, detail="Discussion not found")
 
-    discussion = discussion.get(discussion_id)
-
-    discussion_contacts = discussion.get("contacts", [])
+    discussion_contacts = discussion.get("contacts")
     if user_id not in discussion_contacts:
         raise HTTPException(status_code=404, detail="The user is not part of the discussion")
 
-    message_dict = create_new_message(message_data)
-    message_dict["type"] = "message"
-    return message_dict
+    create_new_message(message_data, user_id)
+
+    messages, error = get_messages_by_discussion_id(user_id, discussion_id)
+    if error:
+        raise HTTPException(status_code=404, detail=error)
+
+    return messages
 
 
-@message_router.get("/api/messages", response_model=List[ResponseMessages])
-def get_message(discussion_data: Discussions, user_id: str = Depends(auth_handler.auth_wrapper)):
-    discussion_id = discussion_data.discussion_id
-    discussion, error = db.get_discussions()
+@message_router.get("/api/messages", response_model=List[MessageResponse])
+def get_messages(discussion_id: UUID, user_id: str = Depends(auth_handler.auth_wrapper)):
+    discussion, error = db.get_discussion(discussion_id)
     if not discussion:
         raise HTTPException(status_code=404, detail="Discussion not found.")
 
-    discussion = discussion.get(discussion_id)
-    discussion_contacts = discussion.get("contacts", [])
+    discussion_contacts = discussion.get("contacts")
     if user_id not in discussion_contacts:
         raise HTTPException(status_code=404, detail="The user is not part of the discussion")
 
--- Messages/utils.py
+++ Messages/utils.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from uuid import uuid4
 from auth import AuthHandler
 from datetime import datetime
@@ -9,26 +8,21 @@ auth_handler = AuthHandler()
 pwd_context = CryptContext(schemes=["bcrypt"])
 
 
-def create_new_message(message_data):
+def create_new_message(message_data, user_id):
     messages, error = db.get_messages()
     message_dict = message_data.model_dump()
     message_id = str(uuid4())
 
     time = datetime.now()
-    hour = time.hour
-    date = time.date()
-    if len(str(time.minute)) == 1:
-        minute = f"0{time.minute}"
-    else:
-        minute = time.minute
+    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
     message_dict["id"] = message_id
     messages[message_id] = message_dict
-    message_dict["created_at"] = f"{date}    {hour}:{minute}"
+    message_dict["created_at"] = formatted_time
 
     db.create_message(
         message_id=message_dict["id"],
         discussion_id=message_dict["discussion_id"],
-        user_id=message_dict["user_id"],
+        user_id=user_id,
         value=message_dict["value"],
         created_at=message_dict["created_at"])
     return message_dict
@@ -38,16 +32,18 @@ def get_messages_by_discussion_id(my_user_id, discussion_id):
     messages, error = db.get_messages()
     users_dict = db.get_users()
     message_list = []
+
     if messages:
         for key in messages:
             message = messages[key]
-            if message.get("discussion_id") == discussion_id:
+            if message.get("discussion_id") == str(discussion_id):
                 user_id = message.get("user_id")
                 if user_id == my_user_id:
                     message["name"] = "You"
                 else:
                     message["name"] = users_dict.get(user_id, {}).get("name")
                 message_list.append(message)
+
         return message_list, None
     else:
         return [], "No messages found"
